ac937dd5579bb113ab57ea7ce0fb471db1e786d0
classfree <classfrees@naver.com>
2014-07-11 17:08:13 +0900

    test

--- src/com/lge/d3/screenrecorder/Configs.java
+++ src/com/lge/d3/screenrecorder/Configs.java
@@ -5,4 +5,5 @@ public class Configs {
 	public static final String KEY_RESOLUTIOM_HEIGHT = "resolution_width";
 	public static final String KEY_BIT_RATE = "bit_rate";
 	public static final String KEY_TIME_LIMIT = "time_limit";
+	public static final String TAG = "ScreenRecorder";
 }
